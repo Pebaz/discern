cbbeb98345a396a7632b1f96771ea5cf6bb250e7
Pebaz <pebazium@gmail.com>
2021-03-04 20:50:10 -0500

    test file

---
+++ test.py
@@ -0,0 +1,21 @@
+import discern as disc
+
+
+print(disc.add(1, 2))
+print(disc.add(1, number2=2))
+
+print(disc.sub(1, 2))
+print(disc.sub(1, number2=2))
+
+print(disc.mul(2, 3))
+print(disc.mul(2, number2=3))
+
+print(disc.div(1, 2))
+print(disc.div(1, number2=2))
+
+print(disc.sum())
+print(disc.sum(1, 2, 3, 4, 5, 6))
+
+print(disc.prod())
+print(disc.prod(1, 2, 3, 4, 5, 6))
+
